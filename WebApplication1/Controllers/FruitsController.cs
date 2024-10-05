7d6113b600f98e4eddd662007bab5ac24b66f891
maaz <maazulhaq00@gmail.com>
2024-10-05 13:08:05 +0500

    Simple apis

---
+++ WebApplication1/Controllers/FruitsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsController : ControllerBase
+    {
+        public List<string> Fruits = new List<string> { "Apple", "Mango", "Orange" };
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return Fruits;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> GetFruitById([FromRoute]int id)
+        {
+            if(id >= Fruits.Count)
+            {
+                return NotFound();
+            }
+            return Ok(Fruits.ElementAt(id));
+
+        }
+        [HttpPost]
+        public List<string> AddFruit([FromBody]string fruit)
+        {
+            Fruits.Add(fruit);
+
+            return Fruits;
+        }
+    }
+}
